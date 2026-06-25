e7ce5d8c82cc19bf3517f4a4702ee570ed930bea
cervantes-jose <jose.cervantes.jco@gmail.com>
2026-06-25 06:29:35 -0400

    sql query 2 cleanup

--- sql/query2_sleep_performance.sql
+++ sql/query2_sleep_performance.sql
@@ -6,19 +6,18 @@ SELECT
         jsonb_array_length(se.sets)
     ) AS total_volume,
     (
-    SELECT AVG(daily_protein)
-    FROM (
-        SELECT SUM(fe.protein) AS daily_protein
-        FROM food_entries fe
-        WHERE fe.user_id = ws.user_id
-        AND fe.date::date BETWEEN ws.date::date - INTERVAL '2 days' AND ws.date::date - INTERVAL '1 day'
-        GROUP BY fe.date
-    ) daily_totals
-) AS avg_prior_protein
+        SELECT me.value
+        FROM measurement_entries me
+        JOIN measurements m ON m.id = me.measurement_id
+        WHERE me.user_id = ws.user_id
+        AND m.name = 'Sleep Score'
+        AND me.date::date = ws.date::date - INTERVAL '1 day'
+        LIMIT 1
+    ) AS prior_sleep_score
 FROM workout_sessions ws
 JOIN session_exercises se
     ON se.session_id = ws.id
     AND se.user_id = ws.user_id
-WHERE ws.user_id = 'a883829b-0653-431b-b138-c425431efbd7'
+WHERE ws.user_id = 'analytics-user-uuid'
 GROUP BY ws.date, ws.user_id
 ORDER BY ws.date;
